fa4a2ce83155b8fb4418c7097c0b7f16a1ea6668
Eduardo da Silva <eduardo.silva@mobiauto.com.br>
2022-09-28 23:35:53 -0300

    forms validation

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,23 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface profileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData = true }: profileProps) {
     return (
-        <Flex align="center">
-        <Box mr="4" textAlign="right">
-          <Text>Eduardo da Silva</Text>
-          <Text 
-            color="gray.300"
-            fontSize="small"
-          >
-            eduardomaea@gmail.com
-          </Text>
-        </Box>
+      <Flex align="center">
+        {showProfileData && (
+          <Box mr="4" textAlign="right">
+            <Text>Eduardo da Silva</Text>
+            <Text 
+              color="gray.300"
+              fontSize="small"
+            >
+              eduardomaea@gmail.com
+            </Text>
+          </Box>
+        )}
 
         <Avatar size="md" name="Eduardo da Silva" src="https://github.com/duhmaea11.png" />
       </Flex>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
-import { Button, Flex, Stack } from "@chakra-ui/react";
+import * as yup from 'yup'
 import { Input } from "../components/Form";
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+
+type SignInFormData = {
+  email: string
+  password: string
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatorio').email('E-mail invalido'),
+  password: yup.string().required('Senha Obrigatória'),
+})
 
 export default function SignIn() {
+  const { register, handleSubmit, formState }= useForm({
+    resolver: yupResolver(signInFormSchema)
+  })
+
+  const { errors } = formState 
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    
+    console.log(values)
+  }
+
   return (
     <Flex 
       w="100vw" 
@@ -17,16 +43,30 @@ export default function SignIn() {
           p="8"
           borderRadius={8}
           flexDir="column"
+          onSubmit={handleSubmit(handleSignIn)}
         >
           <Stack>
-            <Input name="email" type="email" label="E-mail" />
-            <Input name="password" type="password" label="Senha" />
+            <Input 
+              name="email" 
+              type="email" 
+              label="E-mail" 
+              error={errors.email}
+              {...register('email')}
+            />
+            <Input 
+              name="password" 
+              type="password" 
+              label="Senha" 
+              error={errors.password}
+              {...register('password')}  
+            />
           </Stack>
 
-          <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          <Button type="submit" mt="6" colorScheme="pink" size="lg" isLoading={formState.isSubmitting}>
             Entrar
           </Button>
         </Flex>
     </Flex>
   );
 }
+
